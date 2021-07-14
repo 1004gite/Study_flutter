3e2771eae71c69d303b9614f17f8afc40e895c56
1004gite <1004gite@naver.com>
2021-07-14 18:38:58 +0900

    FloatingActionButton 추가

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,28 @@ class HelloPage extends StatefulWidget {
 }
 
 class _HelloPageState extends State<HelloPage> {
+  //flutter에서 변수명 앞 _는 private를 의미한다.
+  int _count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+          onPressed: _addCount
+      ),
       appBar: AppBar(
         //여기서 wiget은 HellpPage 객체를 나타냄, this와 비슷한 개념
         title: Text(widget.title),
       ),
-      body: Text('Flutter Test App', style: TextStyle(fontSize: 30)),
+      body: Text(_count.toString(), style: TextStyle(fontSize: 30)),
     );
+  }
+
+  void _addCount() {
+    //setState함수는 UI 변경을 요청한다.
+    setState(() {
+      _count++;
+    });
   }
 }
