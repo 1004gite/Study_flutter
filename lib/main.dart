c5177c99d5a1810671d0e3f834290004cb4ac6da
1004gite <1004gite@naver.com>
2021-07-14 18:26:32 +0900

    상태변환가능한 위젯 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("flutter Test"),
-        ),
-          body: Text(
-            'welcome',
-            style: TextStyle(fontSize: 30),
-          )),
+      //위쪽에서 기본적인 테마를 정한 후 아래의 내용은 HelloPage에서 관리
+      home: HelloPage('Hello World')
+    );
+  }
+}
+
+
+
+//안드로이드 스튜디오 기준 stf로 상태변경 가능한 위젯 생성
+class HelloPage extends StatefulWidget {
+  final String title;
+
+  HelloPage(this.title) : super();
+
+  @override
+  _HelloPageState createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //여기서 wiget은 HellpPage 객체를 나타냄, this와 비슷한 개념
+        title: Text(widget.title),
+      ),
+      body: Text('Flutter Test App', style: TextStyle(fontSize: 30)),
     );
   }
 }
