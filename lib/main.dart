6c637bd7bb5226cd50a5c32691456b75a1e8c08e
1004gite <1004gite@naver.com>
2021-07-17 05:36:25 +0900

    쿠퍼티노,메테리얼 동시사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,6 +32,7 @@ class HelloPage extends StatefulWidget {
 class _HelloPageState extends State<HelloPage> {
   //flutter에서 변수명 앞 _는 private를 의미한다.
   int _count = 0;
+  bool _cuperSwitchValue = true;
 
   @override
   Widget build(BuildContext context) {
@@ -43,12 +45,30 @@ class _HelloPageState extends State<HelloPage> {
         //여기서 wiget은 HellpPage 객체를 나타냄, this와 비슷한 개념
         title: Text(widget.title),
       ),
-      body: Text(_count.toString(), style: TextStyle(fontSize: 30)),
+      body: Center(
+        child: Column(
+          //새로배열 안에서의 child 들을 가운데정렬
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('클릭한 횟수', style: TextStyle(fontSize: 20)),
+            //toString 사용하지 않고 $로 String안에 변수 삽입 가능
+            Text('$_count', style: TextStyle(fontSize: 30)),
+            CupertinoSwitch(
+                value: _cuperSwitchValue,
+                onChanged: (bool v) {
+                  setState(() {
+                    _cuperSwitchValue = v;
+                  });
+                }
+            )
+          ],
+        ),
+      )
     );
   }
 
   void _addCount() {
-    //setState함수는 UI 변경을 요청한다
+    //setState함수는 UI 변경을 요청한다.
     setState(() {
       _count++;
     });
